4ab358b709a5ca23c5490fc0353003fb07f7c20d
jeongin00 <hjeongin1001@gmail.com>
2025-08-30 23:20:45 +0900

    Add files via upload

---
+++ src/test/java/com/risingcamp/grittest/GrittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risingcamp.grittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
